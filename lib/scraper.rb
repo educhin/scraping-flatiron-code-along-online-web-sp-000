310c67ce3892cec444f2cff17e59d21843e82e36
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-26 01:31:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,22 +18,20 @@ class Scraper
   end
 
   def get_page
-    doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    doc.css(".post").each do |post|
+    Nokogiri::HTML(open("https://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    .css(".post").each do |post|
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
     end
-    binding.pry
-  end
-
-  def get_courses
-    Course.all
-  end
-
-  def make_courses
-
   end
 end
 
