bc6254e5767a026df02dd42a6732bcfd02aeb817
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-26 01:15:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
@@ -17,8 +18,7 @@ class Scraper
   end
 
   def get_page
-    html = open("https://learn-co-curriculum.github.io/site-for-scraping/courses")
-    doc = Nokogiri::HTML(html)
+    doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
   def get_courses
